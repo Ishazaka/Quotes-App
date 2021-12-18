9bcc7d1b43ad9aadcb89be212e2137e75cc3191b
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-19 04:08:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+const quotes = [
+    {
+    quote: `“The best way to appreciate your job is to imagine yourself without one.”`,
+    author: `~ Oscar Wilde`
+
+  },
+{
+ quote: `“All our dreams can come true, if we have the courage to pursue them.”`,
+    author: `~  Walt Disney`
+
+},
+  {
+    quote: `"I can give you a six-word formula for success: Think things through--then follow through."`,
+    author: `~ Edward Rickenbacker`
+  },
